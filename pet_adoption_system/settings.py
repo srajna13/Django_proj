656d3fcb765c55c820d43f3037b882d01970b256
Your Name <you@example.com>
2025-04-05 14:53:42 +0530

    add .gitignore

--- pet_adoption_system/settings.py
+++ pet_adoption_system/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fh9j-@qhrdtqv4!50^v9ihn4qoo0rd8x9s1s2j6zfm87n18v=$"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -79,11 +80,11 @@ WSGI_APPLICATION = "pet_adoption_system.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "pet_adoption_db",
-        'USER': 'root',
-        'PASSWORD': 'Abcd@1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        "NAME": config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
     }
 }
 
